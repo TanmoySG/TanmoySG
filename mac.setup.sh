6fb85497b35baf8938e002004965252ea5bc265c
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-07-27 22:57:34 +0530

    Create mac.setup.sh

---
+++ mac.setup.sh
@@ -0,0 +1,15 @@
+# My macOS setup for local development
+
+# Setup brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/tanmoysg/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+which brew
+
+# Setup VS Code
+brew install --cask visual-studio-code
+which code
+
+# Setup Go
+brew install go
+which go
